a44ff79295229692cd1ff9ac1248d58abe56b60b
Georgi Valchanov <valchanov.biz@gmail.com>
2024-01-23 15:24:57 +0200

    UserService added UserServiceImpl added

---
+++ ForumTeamProject/forum/src/main/java/org/forum/web/forum/Controllers/UserRestController.java
@@ -0,0 +1,11 @@
+//package org.forum.web.forum.Controllers;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/users")
+//public class UserRestController {
+//
+//
+//}
