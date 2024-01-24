b19cb615caed21789cb6d8cd86c87d5cf9471134
Georgi Valchanov <valchanov.biz@gmail.com>
2024-01-24 18:15:32 +0200

    create.sql updated

--- ForumTeamProject/forum/db/create.sql
+++ ForumTeamProject/forum/db/create.sql
@@ -1,3 +1,17 @@
+create table phone_numers
+(
+    phone_number_id int auto_increment
+        primary key,
+    phone_number    varchar(15) not null
+);
+
+create table tags
+(
+    tag_id   int auto_increment
+        primary key,
+    tag_name int not null
+);
+
 create table users
 (
     user_id      int auto_increment
@@ -41,3 +55,76 @@ create table comments
     constraint comments_users_user_id_fk
         foreign key (created_by_id) references users (user_id)
 );
+
+create table disliked_posts
+(
+    disliked_posts_id int auto_increment
+        primary key,
+    post_id           int not null,
+    user_id           int not null,
+    constraint disliked_posts_posts_post_id_fk
+        foreign key (post_id) references posts (post_id),
+    constraint disliked_posts_users_user_id_fk
+        foreign key (user_id) references users (user_id)
+);
+
+create table liked_posts
+(
+    liked_posts_id int auto_increment
+        primary key,
+    post_id        int not null,
+    user_id        int not null,
+    constraint liked_posts_posts_post_id_fk
+        foreign key (post_id) references posts (post_id),
+    constraint liked_posts_users_user_id_fk
+        foreign key (user_id) references users (user_id)
+);
+
+create table posts_tags
+(
+    posts_tags_id int auto_increment
+        primary key,
+    post_id       int not null,
+    tag_id        int not null,
+    constraint posts_tags_posts_post_id_fk
+        foreign key (post_id) references posts (post_id),
+    constraint posts_tags_tags_tag_id_fk
+        foreign key (tag_id) references tags (tag_id)
+);
+
+create table users_comments
+(
+    users_comments_id int auto_increment
+        primary key,
+    user_id           int not null,
+    comment_id        int not null,
+    constraint users_comments_comments_comment_id_fk
+        foreign key (comment_id) references comments (comment_id),
+    constraint users_comments_users_user_id_fk
+        foreign key (user_id) references users (user_id)
+);
+
+create table users_phone_numbers
+(
+    users_phone_numbers_id int auto_increment
+        primary key,
+    user_id                int not null,
+    phone_number_id        int not null,
+    constraint users_phone_numbers_phone_numers_phone_number_id_fk
+        foreign key (phone_number_id) references phone_numers (phone_number_id),
+    constraint users_phone_numbers_users_user_id_fk
+        foreign key (user_id) references users (user_id)
+);
+
+create table users_posts
+(
+    users_posts_id int auto_increment
+        primary key,
+    user_id        int not null,
+    post_id        int not null,
+    constraint users_posts_posts_post_id_fk
+        foreign key (post_id) references posts (post_id),
+    constraint users_posts_users_user_id_fk
+        foreign key (user_id) references users (user_id)
+);
+
