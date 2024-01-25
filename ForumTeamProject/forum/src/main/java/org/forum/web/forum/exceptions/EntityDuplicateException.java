1e959a975ddaa208685e96160e9310a6388b57ec
Georgi Valchanov <valchanov.biz@gmail.com>
2024-01-25 15:39:31 +0200

    EntityDuplicateException added.

---
+++ ForumTeamProject/forum/src/main/java/org/forum/web/forum/exceptions/EntityDuplicateException.java
@@ -0,0 +1,8 @@
+package org.forum.web.forum.exceptions;
+
+public class EntityDuplicateException extends RuntimeException {
+
+    public EntityDuplicateException(String type, String attribute, String value) {
+        super(String.format("%s with %s %s already exists.", type, attribute, value));
+    }
+}
