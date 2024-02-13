50afa348eb1ceee9b56b7a82fb0f1cb7b1dfc0ae
Georgi Valchanov <valchanov.biz@gmail.com>
2024-02-13 17:59:18 +0200

    Create scheme script updated

--- ForumTeamProject/forum/db/create.sql
+++ ForumTeamProject/forum/db/create.sql
@@ -9,13 +9,14 @@ create table users
 (
     user_id    int auto_increment
         primary key,
-    first_name varchar(32)  not null,
-    last_name  varchar(32)  not null,
-    username   varchar(50)  not null,
-    password   varchar(20)  not null,
-    email      varchar(100) not null,
-    is_admin   tinyint(1)   null,
-    is_banned  tinyint(1)   null
+    first_name varchar(32)                                         not null,
+    last_name  varchar(32)                                         not null,
+    username   varchar(50)                                         not null,
+    password   varchar(20)                                         not null,
+    email      varchar(100)                                        not null,
+    is_admin   tinyint(1)                                          null,
+    is_banned  tinyint(1)                                          null,
+    photo_url  varchar(320) default 'assets/users/defaultUser.png' null
 );
 
 create table phone_numbers
@@ -103,16 +104,6 @@ create table users_comments
     constraint users_comments_comments_comment_id_fk
         foreign key (comment_id) references comments (comment_id),
     constraint users_comments_users_user_id_fk
-        foreign key (user_id) references users (user_id)
-);
-
-create table users_photos
-(
-    user_photo_id int  not null
-        primary key,
-    photo         blob null,
-    user_id       int  null,
-    constraint users_photos_users_user_id_fk
         foreign key (user_id) references users (user_id)
 );
 
